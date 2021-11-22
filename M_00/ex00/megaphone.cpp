292dc10b6407cc0c2952aec7fd4f4fc461a9c397
BooDeer <hsissi3a@gmail.com>
2021-11-22 13:39:23 +0100

    done

--- M_00/ex00/megaphone.cpp
+++ M_00/ex00/megaphone.cpp
@@ -1,29 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 11:57:11 by hboudhir          #+#    #+#             */
+/*   Updated: 2021/11/22 12:54:20 by hboudhir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
-
-char	*toupper_string(char *str);
-
-int	main(int argc, char *argv[])
+int	main(int ac, char **av)
 {
-	int	i;
-
-	i = 0;
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";	
+	std::string tmp;
+	
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-		while (++i < argc)
-			std::cout << toupper_string(argv[i]);
+		for (int i = 1; av[i]; i++)
+			for (int j = 0; av[i][j]; j++)
+				std::cout << (char)toupper(av[i][j]);
 	std::cout << std::endl;
-	return (0);
-}
-
-
-char	*toupper_string(char *str)
-{
-	int	i;
-
-	i = -1;
-
-	while (str[++i])
-		str[i] = ::toupper(str[i]);
-	return (str);
 }
