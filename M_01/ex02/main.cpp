f4d25ef2ecc9622a710b459c07ebcc52e876fc6e
BooDeer <hsissi3a@gmail.com>
2021-11-26 18:08:51 +0100

    day01 ex02 done

--- M_01/ex02/main.cpp
+++ M_01/ex02/main.cpp
@@ -1,22 +1,41 @@
-#include "Zombie.hpp"
-#include "ZombieEvent.hpp"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/26 17:21:31 by hboudhir          #+#    #+#             */
+/*   Updated: 2021/11/26 18:05:49 by hboudhir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	main()
+#include <iostream>
+
+int main( void )
 {
-	Zombie *zombo1 = new Zombie("ChumpChump", "dead");
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
 	
-	zombo1->announce();
-
-	ZombieEvent *zombo2 = new ZombieEvent();
-	zombo2->setZombieType("walking dead");
-	zombo2->randomChump();
-
-	Zombie *zombo3 = zombo2->newZombie("The last zombie");
-	zombo3->announce();
-
-
-	delete zombo1;
-	delete zombo2;
-	delete zombo3;
-
+	(void)stringPTR;
+	std::cout << "Address of the string using [str]       : " << &str << std::endl;
+	std::cout << "Address of the string using [stringREF] : " << &stringREF << std::endl;
+	std::cout << "Address of the string using [stringPTR] : " << stringPTR << std::endl;
+	std::cout << std::endl << "=============================================================" << std::endl << std::endl;
+	std::cout << "[str] value using [stringREF]  : " << stringREF << std::endl;
+	std::cout << "[str] value using [stringPTR]  : " << *stringPTR << std::endl << std::endl;
 }
+/*
+int a = 21;
+int* aPTR = &a;
+int& aREF = a; // v
+
+aREF = a; // x
+==> a = 21
+a = 42;
+==> aREF = 42
+aPTR = <address>;
+*aPTR ==> <value address &a> ==> a
+
+*/
