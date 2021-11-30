32428f54385d039cb431bed2745fbaa939526668
BooDeer <hsissi3a@gmail.com>
2021-11-30 17:40:54 +0100

    ex 01 day 02 done

---
+++ M_02/ex01/Fixed.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 08:39:02 by hboudhir          #+#    #+#             */
+/*   Updated: 2021/11/30 17:37:12 by hboudhir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed ( void ) : _fp(0)
+{
+	std::cout << "Default constructor called" << std::endl;	
+}
+
+Fixed::Fixed (const Fixed& src )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+Fixed::Fixed ( const int i )
+{
+	std::cout << "Int constructor called" << std::endl;
+	_fp = i * (float)(1 << _nBit);
+}
+
+Fixed::Fixed ( const float f)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fp = roundf(f * (float)(1 << _nBit));
+}
+
+Fixed::~Fixed ( void )
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator= (Fixed const& rhs )
+{
+	std::cout << "Assignation operator called" << std::endl;
+
+	if (this != &rhs)
+		this->_fp = rhs._fp;
+	return *this;
+}
+int	Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _fp;
+}
+
+void	Fixed::setRawBits ( int const raw )
+{
+	_fp = raw;
+}
+
+float	Fixed::toFloat( void ) const
+{
+	return (_fp / (float)(1 << _nBit));
+}
+
+int		Fixed::toInt ( void ) const
+{
+	return (_fp / (float)(1 << _nBit));
+}
+
+std::ostream & operator<<( std::ostream & o,  Fixed const& rhs)
+{
+	o << rhs.toFloat();
+	return o;
+}
---
+++ M_02/ex01/Fixed.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 08:39:11 by hboudhir          #+#    #+#             */
+/*   Updated: 2021/11/30 17:31:55 by hboudhir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+/*
+	The class goes as follow:
+		- Constructors ==> Destructors ==> Operator overloaders ==> Methods.
+*/
+class	Fixed {
+	public:
+		Fixed ( void ); 					// canonical
+		Fixed (const Fixed& src ); 				// canonical
+		Fixed ( const int i );
+		Fixed ( const float f);
+		~Fixed( void );						// canonical
+		Fixed& operator=( Fixed const& rhs);	// canonical
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+		float	toFloat ( void ) const;
+		int		toInt( void ) const;
+	private:
+		static const int	_nBit = 8;
+		int					_fp;
+};
+
+std::ostream & operator<<( std::ostream & o,  Fixed const& rhs);
+#endif
