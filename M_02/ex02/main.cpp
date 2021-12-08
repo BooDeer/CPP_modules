b9a2ee566b2ce2bddd9637eb1ccb0357cdd7adcd
BooDeer <hsissi3a@gmail.com>
2021-12-08 11:45:07 +0100

    fast fix

---
+++ M_02/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboudhir <hboudhir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 10:32:37 by hboudhir          #+#    #+#             */
+/*   Updated: 2021/12/02 08:38:52 by hboudhir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main ( void )
+{
+	// Fixed		a;
+	// Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ));
+	
+	// std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	// std::cout << a << std::endl;
+	
+	
+	// std::cout << b << std::endl;
+	// std::cout << Fixed::max( a, b ) << std::endl;
+	
+	return (0);
+}
